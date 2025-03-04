df6f70296be3801e0e22a412c643900d6f975aef
14 <14@DESKTOP-5DQ9QDI>
2025-03-04 17:14:43 +0900

    14

--- src/M/Ex1.java
+++ src/M/Ex1.java
@@ -1,10 +1,23 @@
 package M;
 
 public class Ex1 {
-
+//Ex1 - 컴파일 오류
+//Ex2 - 런타임 오류
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int value;
+		
+		int[] arr =  new int[5];
+		arr[5] = 5;
+		
+		
 
 	}
 
 }
+
+//예외 처리과정
+//에러발생
+//에러와 일치하는 catch 블럭을 찾는다
+//catch 블록을 들어가 코드를 실행한다
+//남은 프르그램 이어 실행
